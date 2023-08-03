264513506ed7fae2b8b02ee4fb89b4fa50b3a454
alexgm5555 <alexgm5555@hotmail.com>
2023-08-03 14:46:45 -0500

    Feat: document api swaegger v3

--- src/auth/entities/user.entity .ts
+++ src/auth/entities/user.entity .ts
@@ -1,29 +1,36 @@
 import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Product } from '../../products/entities';
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+
+  @ApiProperty()// para que aparezca el campo en swaegger
   @Column('text', {
     unique: true,
   })
   email: string;
 
+  @ApiProperty()// para que aparezca el campo en swaegger
   @Column('text',{
     select:  false // se pone para que no aparezca en el find ya que e un dato sensible 
   })
   password: string;
 
+  @ApiProperty()// para que aparezca el campo en swaegger
   @Column('text')
   fullName:string;
 
+  @ApiProperty()// para que aparezca el campo en swaegger
   @Column('bool', {
     default: true,
   })
   isActive: boolean;
 
+  @ApiProperty()// para que aparezca el campo en swaegger
   @Column('text', {
     array: true,
     default: ['user'],// para que cuando se qcree solo quede uno por defecto user
